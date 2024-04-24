bb5495b515acfdbf48d36e71de6982e180a92f5b
ChrisPytel <chris_pytel@hotmail.com>
2024-04-24 18:30:24 -0400

    update search.js script

---
+++ public/scripts/search.js
@@ -0,0 +1,19 @@
+// Client facing scripts related to search.ejs run here
+
+$(() => {
+  console.log("Successfully loaded jQuery on search.js script attached to search page");
+  
+  // our code goes here
+  
+
+
+
+
+
+
+
+
+
+
+
+});
