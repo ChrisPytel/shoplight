69138aad073a0dc0a8f59bec8cde858ae5999eba
Cynthia <cynthia-lam@outlook.com>
2024-04-23 20:38:57 -0400

    Create schema for tables users, products, orders, favourites, messages

---
+++ db/schema/02_products.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS products CASCADE;
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  description text,
+  photo_url VARCHAR(255) NOT NULL,
+  price INTEGER NOT NULL,
+  is_featured BOOLEAN,
+  is_available BOOLEAN,
+  date_posted TIMESTAMP default CURRENT_TIMESTAMP
+);
