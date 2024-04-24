89dc4ff251dd332fab8311c7c1addddab3a1b118
Cynthia <cynthia-lam@outlook.com>
2024-04-23 20:39:23 -0400

    Create seeds for tables users, products, orders, favourites, messages

---
+++ db/seeds/03_orders.sql
@@ -0,0 +1,14 @@
+INSERT INTO orders (user_id, product_id) 
+VALUES (1, 1);
+
+INSERT INTO orders (user_id, product_id) 
+VALUES (2, 3);
+
+INSERT INTO orders (user_id, product_id) 
+VALUES (3, 2);
+
+INSERT INTO orders (user_id, product_id) 
+VALUES (1, 4);
+
+INSERT INTO orders (user_id, product_id) 
+VALUES (2, 5);
