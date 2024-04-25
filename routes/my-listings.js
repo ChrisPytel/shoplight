f3195dd84191950149ed1133d9a7975ee797f526
Cynthia <cynthia-lam@outlook.com>
2024-04-24 23:41:34 -0400

    Update GET to render items from db and send to ejs template

--- routes/my-listings.js
+++ routes/my-listings.js
@@ -6,24 +6,45 @@
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
+const db = require('../db/connection');
 
 //Renders the my-listings page
-router.get('/', (req, res) => {  
+router.get('/', (req, res) => {
   console.log("GET my-listings entered");
-
-  res.render('my-listings');
-}); 
+  return db
+  .query(`SELECT * FROM products;`)  //replace this query with one that will retreieve only products that match user_in of person logged in                                                                            //note: we need to know what object is being given here in order to add each value
+  .then((products) => {
+    const templateVars = {
+      listings : products.rows
+    };
+    console.log(templateVars);
+    return res.render('my-listings', templateVars);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+  
+});
 
 
 //Handles any post requests on my-listings
-/*   router.post("/", (req, res) => {
-  // our code goes here
+// router.post("/", (req, res) => {
+// return db
+//   .query(`INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
+// VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`,
+//     ["7 values go here"])                                                                              //note: we need to know what object is being given here in order to add each value
+//   .then((result) => {
+//     return result.rows[0];
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
 
+// const templateVars = { }; //update this line with the new db
+// return res.render("my-listings", templateVars);
+//   });
 
-
-    
-  }); */
 
 module.exports = router;
