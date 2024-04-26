13e4adf1a46d4c373fddd3414fc891a6618ea6bf
ChrisPytel <chris_pytel@hotmail.com>
2024-04-26 18:15:23 -0400

    add cookie functionality to other routes

--- routes/messages.js
+++ routes/messages.js
@@ -8,6 +8,12 @@
 const express = require('express');
 const router  = express.Router();
 
+const cookieSession = require('cookie-session');
+router.use(cookieSession({
+  name: 'session',
+  keys: ['superSecretKey', 'superSecretKey2'], /* secret keys */
+  maxAge: 24 * 60 * 60 * 1000 // Cookie Options (24 hours)
+}));
 
 //Renders the messages page
 router.get('/', (req, res) => {  
