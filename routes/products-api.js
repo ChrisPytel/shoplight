b9e3bd7de2b7a1af9de16d253e3f0dd41eb92c17
Lubi Islam <lubaba.islam91@gmail.com>
2024-04-26 21:15:53 -0400

    Update GET request to pull product details from queries/products

--- routes/products-api.js
+++ routes/products-api.js
@@ -2,14 +2,24 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/queries/products');
 
-router.get('/', async (req, res) => {
-  try {
-    const products = await db.getProducts();
-    res.json(products);
-  } catch (error) {
-    console.error('Error fetching products:', error);
-    res.status(500).send('Internal Server Error');
-  }
+const cookieSession = require('cookie-session');
+router.use(cookieSession({
+  name: 'session',
+  keys: ['superSecretKey', 'superSecretKey2'], /* secret keys */
+  maxAge: 24 * 60 * 60 * 1000 // Cookie Options (24 hours)
+}));
+
+router.get('/', (req, res) => {
+
+  return db.getProducts()
+  .then((products) => {
+        const templateVars = {
+        listings: products
+        // put call here for featured
+      };
+      console.log(products);
+      return res.render('index.ejs', templateVars);
+  })
 });
 
 module.exports = router;
