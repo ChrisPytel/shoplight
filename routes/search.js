b0f875be5922560585827d5843ca16cf62d112ef
Cynthia <cynthia-lam@outlook.com>
2024-04-28 21:22:09 -0400

    Add GET and POST

--- routes/search.js
+++ routes/search.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db/queries/products');
+
 const queryUser = require('../db/queries/getUserByID');
+const searchListingsFn = require('../db/queries/searchListings');
 
 const cookieSession = require('cookie-session');
 router.use(cookieSession({
@@ -13,9 +14,29 @@ router.use(cookieSession({
 router.get('/', (req, res) => {
   const cookieStored = req.session.user_id;
   const displayNamePromise = queryUser.getUserByID(cookieStored);   // Retrieve displayName asynchronously
-  const productsPromise = db.getProducts();                         // Retrieve products
+  const searchPromise = searchListingsFn.searchListings(req.body); 
 
-  Promise.all([displayNamePromise, productsPromise])                // Execute both promises concurrently using Promise.all
+  Promise.all([displayNamePromise, searchPromise])                // Execute both promises concurrently using Promise.all
+      .then(([displayName, products]) => {
+          const templateVars = {
+              cookieStored,
+              displayName,
+              listings: products
+          };
+          return res.render('search.ejs', templateVars);
+      })
+      .catch((error) => {
+          console.error('Error fetching data:', error);
+          res.status(500).send('Internal Server Error');
+      });
+});
+
+router.post('/', (req, res) => {
+  const cookieStored = req.session.user_id;
+  const displayNamePromise = queryUser.getUserByID(cookieStored);   // Retrieve displayName asynchronously
+  const searchPromise = searchListingsFn.searchListings(req.body); 
+
+  Promise.all([displayNamePromise, searchPromise])                // Execute both promises concurrently using Promise.all
       .then(([displayName, products]) => {
           const templateVars = {
               cookieStored,
