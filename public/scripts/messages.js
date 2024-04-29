579c2e7770415117d06cdf35595f9c0b32700a83
ChrisPytel <chris_pytel@hotmail.com>
2024-04-29 00:49:56 -0400

    update ajax GET query to start rendering chain

--- public/scripts/messages.js
+++ public/scripts/messages.js
@@ -1,20 +1,62 @@
 // Client facing scripts related to messages.ejs run here
 
-$(() => {
+// const fn = require('../../resources/functions');  // DOM ERROR returns messages.js:3 Uncaught ReferenceError: require is not defined
+
+$(document).ready(function() {
   console.log("Successfully loaded jQuery on messages.js script attached to messages.ejs");
+    
+  $('h1').on('click', function(event){
+    console.log(`Refreshed Inbox!`);
+    fetchMail();   
+  });
 
-  // our code goes here
+  const renderInboxItems = function(mailObj) {
+    console.log(`Our mailObj is: `, mailObj);
+    mailObj.forEach((message) => {
+      const entry = markupInboxEntry(message);
+      $('.inbox').append(entry); // takes return value and appends it to the top of the tweets container
+    });
+  };
+  
+  //Creates the HTML markup to be appended later to the HTML
+  const markupInboxEntry = function(message) {
+   console.log(`Our message is: `, message);  
+
+    return $(`
+    <article class ="inbox-entry">      
+      <div>
+        <h4>Message from: </h4>
+        <p>Re: </p>
+      </div>
+      <div>
+        <h4>Sent on: </h4>
+        <p>Status: </p>
+      </div>  
+    </article>
+    `);    
+  };
 
 
 
 
 
 
+const fetchMail = () => {
+  $.ajax({
+    url: `/api/messages`,
+    method: 'GET',  // HTTP methods are: 'GET', 'POST', 'PUT', 'DELETE'
+    success: function(dataOnSuccess){
+      console.log(`$.ajax GET request came through. Data is: `, dataOnSuccess);       
+      $('.inbox').empty(); // Purges any old Inbox Entries from page  
+      renderInboxItems(dataOnSuccess.messages);
+    },  
+    error: function(error){
+      console.error(`$.ajax ${this.method} request error on route: '${this.url}'\nDetails:`, error);
+    }  
+  });   
+};  
+
+fetchMail();
 
 
-
-
-
-
-});
-
+}); // --------- end of $(document).ready ---------
