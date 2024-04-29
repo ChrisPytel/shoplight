829eb7c97e3aca10c8f90c8c8152dedf53a5988d
ChrisPytel <chris_pytel@hotmail.com>
2024-04-28 20:03:21 -0400

    include date_sent as part of message table schema and seeds

--- db/seeds/05_messages.sql
+++ db/seeds/05_messages.sql
@@ -4,11 +4,12 @@ INSERT INTO messages (message, user_id_to, user_id_from, product_id) VALUES ('Gr
 INSERT INTO messages (message, user_id_to, user_id_from, product_id) VALUES ('When would you like to meet?', 1, 9, 4);
 INSERT INTO messages (message, user_id_to, user_id_from, product_id) VALUES ('Thanks! Looking forward to it.', 2, 8, 5);
 
-INSERT INTO messages (message, user_id_to, user_id_from, product_id) VALUES ('I''m interested in this item, let me know if its still available', 1, 7, 2);
-INSERT INTO messages (message, user_id_to, user_id_from, product_id) VALUES ('How much are you willing to negotiate a drop on this item?', 1, 2, 2);
-INSERT INTO messages (message, user_id_to, user_id_from, product_id) VALUES ('I''d like to come by to see the item, when are you available?', 1, 8, 5);
-INSERT INTO messages (message, user_id_to, user_id_from, product_id) VALUES ('I''m interested in this item, let me know if its still available', 1, 2, 5);
-INSERT INTO messages (message, user_id_to, user_id_from, product_id) VALUES ('How much are you willing to negotiate a drop on this item?',2 , 3, 1);
-INSERT INTO messages (message, user_id_to, user_id_from, product_id) VALUES ('I''d like to come by to see the item, when are you available?', 2, 4, 1);
-INSERT INTO messages (message, user_id_to, user_id_from, product_id) VALUES ('Is this item still available? I''m interested.', 2, 3, 4);
-INSERT INTO messages (message, user_id_to, user_id_from, product_id) VALUES ('Could you provide more details about the condition of the item?', 2, 5, 2);
+INSERT INTO messages (message, user_id_to, user_id_from, product_id, date_sent) VALUES ('I''m interested in this item, let me know if its still available', 1, 7, 2, '2024-01-12 23:59:59');
+INSERT INTO messages (message, user_id_to, user_id_from, product_id, date_sent) VALUES ('How much are you willing to negotiate a drop on this item?', 1, 2, 2, '2024-01-31 23:59:59');
+INSERT INTO messages (message, user_id_to, user_id_from, product_id, date_sent) VALUES ('I''d like to come by to see the item, when are you available?', 1, 8, 5, '2024-02-15 12:00:00');
+INSERT INTO messages (message, user_id_to, user_id_from, product_id, date_sent) VALUES ('I''m interested in this item, let me know if its still available', 1, 2, 5, '2024-03-05 18:30:00');
+INSERT INTO messages (message, user_id_to, user_id_from, product_id, date_sent) VALUES ('How much are you willing to negotiate a drop on this item?',1 , 3, 1, '2024-04-20 09:45:00');
+INSERT INTO messages (message, user_id_to, user_id_from, product_id, date_sent) VALUES ('I''d like to come by to see the item, when are you available?', 2, 4, 1, '2024-05-10 07:15:00');
+INSERT INTO messages (message, user_id_to, user_id_from, product_id, date_sent) VALUES ('Is this item still available? I''m interested.', 2, 3, 4, '2024-06-25 14:20:00');
+INSERT INTO messages (message, user_id_to, user_id_from, product_id, date_sent) VALUES ('Could you provide more details about the condition of the item?', 2, 5, 2, '2024-07-12 21:00:00');
+INSERT INTO messages (message, user_id_to, user_id_from, product_id, date_sent) VALUES ('I''d like to see more photos of the item you have available.', 1, 4, 1, '2024-01-31 23:59:59');
