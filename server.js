64f4f39d2c2aad519b35626896a4aef7edb57ce3
ChrisPytel <chris_pytel@hotmail.com>
2024-04-30 00:50:57 -0400

    update route sources in server.js

--- server.js
+++ server.js
@@ -28,40 +28,44 @@ app.use(express.static('public'));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
+
+//------------------- API route source ------------------- 
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
-const usersRoutes = require('./routes/users'); // sample route
+const messageApiRoutes = require('./routes/messages-api');
 
+//------------------- Page route source ------------------- 
+const usersRoutes = require('./routes/users');
 const loginRoutes = require('./routes/login');
 const registerRoutes = require('./routes/register');
 const logoutRoute = require('./routes/logout');
 const myListingsRoutes = require('./routes/my-listings');
 const searchRoutes = require('./routes/search');
-
 const messagesRoutes = require('./routes/messages');
-const messageApiRoutes = require('./routes/messages-api'); //for getting/posting our m
+
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
-app.use('/api/users', userApiRoutes);
-app.use('/api/widgets', widgetApiRoutes);
-app.use('/users', usersRoutes);
+// Note: mount other resources here, using the same pattern
 
-app.use('/login', loginRoutes);
-app.use('/register', registerRoutes);
-app.use('/logout', logoutRoute);
-app.use('/my-listings', myListingsRoutes);
-app.use('/search', searchRoutes);
+//------------------- API endpoints ------------------- 
+app.use('/api/users', userApiRoutes);          //sample route
+app.use('/api/widgets', widgetApiRoutes);      //sample route
+app.use('/api/messages', messageApiRoutes);    //for GET/POST-ing our messages to/from DB
 
-app.use('/messages', messagesRoutes);
-app.use('/api/messages', messageApiRoutes);
+//------------------- Page endpoints  ------------------- 
+app.use('/users', usersRoutes);                // sample route
+app.use('/login', loginRoutes);                // GET renders page | POST logs user in + sets cookie
+app.use('/register', registerRoutes);          // GET renders page | POST registers user + sets cookie   <- implement
+app.use('/logout', logoutRoute);               // POST logs user out + clears cookie
+app.use('/my-listings', myListingsRoutes);     // GET renders page
+app.use('/search', searchRoutes);              // GET renders page
+app.use('/messages', messagesRoutes);          // GET renders page | POST directs ajax query to messages-api
 
-// Note: mount other resources here, using the same pattern above
 
 // Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
+// Warning: avoid creating more routes in this file!  Separate them into separate routes files (see above).
 
 app.get('/', (req, res) => {
   res.render('index');
