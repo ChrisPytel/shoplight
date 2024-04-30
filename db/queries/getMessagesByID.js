24ae99fdf4fda6c2cdbb7d8b379565792ffaa385
ChrisPytel <chris_pytel@hotmail.com>
2024-04-30 14:33:31 -0400

    update database queries

--- db/queries/getMessagesByID.js
+++ db/queries/getMessagesByID.js
@@ -3,7 +3,7 @@ const db = require('../connection');
 const getMessagesByID = (id) => {
   console.log(`Our getMessagesByID ID number is: `, id);
   let queryString =`
-  SELECT user_id_to, user_id_from, message, users.name AS from, products.name AS listing, date_sent, read_status
+  SELECT user_id_to, user_id_from, message, users.name AS from, products.name AS listing, products.id AS product_id, date_sent, read_status
   FROM messages
   LEFT JOIN users ON messages.user_id_from = users.id
   LEFT JOIN products ON messages.product_id = products.id
--- db/queries/sendMessage.js
+++ db/queries/sendMessage.js
@@ -2,7 +2,7 @@
 const db = require('../connection');
 
 const sendMessage = (message, personTo, personFrom, productID) => {
-  console.log(`Sending message to DB: `, message, personTo, personFrom, productID);
+  // console.log(`Sending message to DB: `, message, personTo, personFrom, productID);
   let queryArray = [message, personTo, personFrom, productID]
 
   let queryString =`
@@ -10,25 +10,14 @@ const sendMessage = (message, personTo, personFrom, productID) => {
   (message, user_id_to, user_id_from, product_id) 
   VALUES ($1, $2, $3, $4);`;
   return db.query(queryString, queryArray)
-    .then(data => {
-      console.log(`Sucessfully POSTED to DB: `, message, `\nTo accountID# ${personTo} from ID#${personFrom}, regarding listing #${productID}`);
+    .then(() => {
+      console.log(`Sucessfully POSTED to DB: `, queryArray);
     })
     .catch(err => {
       console.log(`Got an error during sendMessage query:\n`, err);
-      // return null;
     })
 };
 
 module.exports = { 
   sendMessage
 };
-
-
-
-/* 
-
-INSERT INTO messages (message, user_id_to, user_id_from, product_id, date_sent) 
-VALUES ('I''m interested in this item, let me know if its still available', 1, 7, 2, '2024-01-12 23:59:59'); 
-
-
-*/
