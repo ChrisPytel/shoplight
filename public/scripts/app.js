8a1bee4d8d249addf49e8973164e42b49cfc669a
Lubi Islam <lubaba.islam91@gmail.com>
2024-04-30 23:06:04 -0400

    Add toggle form function for message seller button on index

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,12 @@
 // Client facing scripts here
+
+// Message Seller toggle button on product index
+function toggleForm() {
+  var form = document.getElementById("myForm");
+  if (form.style.display === "block") {
+    form.style.display = "none"; // If form is visible, hide it
+  } else {
+    form.style.display = "block"; // If form is hidden, show it
+  }
+}
+
