a29fd97ffce9116c794d9ea74b0810636d22be64
ChrisPytel <chris_pytel@hotmail.com>
2024-05-02 15:53:58 -0400

    update DB seeds for users table and fix small typo on getUserByID query

--- db/queries/getUserByID.js
+++ db/queries/getUserByID.js
@@ -3,13 +3,13 @@ const db = require('../connection');
 const getUserByID = (id) => {
   // console.log(`Our id is: `, id);
   let queryString =`
-  SELECT name, city
+  SELECT name, city, community
   FROM users
   WHERE id = $1;`
   return db.query(queryString,[id])
     .then(data => {
       console.log(`Our DB returned: `, data.rows);
-      return data.rows[0]; //returns the name corresponding the ID number of the stored cookie
+      return data.rows[0]; //returns the name, city, and community corresponding the ID number of the stored cookie
     })
     .catch(err => {
       console.log(`Got an error during getUserByID query:\n`, err);
