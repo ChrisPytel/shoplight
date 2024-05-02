b08120adac75f01662da5d91b72facd4158c45d0
ChrisPytel <chris_pytel@hotmail.com>
2024-05-02 15:51:20 -0400

    update getUserByID query to return city, community as well as name

--- db/queries/getUserByID.js
+++ db/queries/getUserByID.js
@@ -3,13 +3,13 @@ const db = require('../connection');
 const getUserByID = (id) => {
   // console.log(`Our id is: `, id);
   let queryString =`
-  SELECT name
+  SELECT name, city
   FROM users
   WHERE id = $1;`
   return db.query(queryString,[id])
     .then(data => {
       console.log(`Our DB returned: `, data.rows);
-      return data.rows[0].name; //returns the name corresponding the ID number of the stored cookie
+      return data.rows[0]; //returns the name corresponding the ID number of the stored cookie
     })
     .catch(err => {
       console.log(`Got an error during getUserByID query:\n`, err);
