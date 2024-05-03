a57522fbcb3acffb8cdbb17f7165de993bba8192
Lubi Islam <lubaba.islam91@gmail.com>
2024-05-02 23:59:49 -0400

    Add route to post new product to favorites page

---
+++ routes/add-to-favorites.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+const db = require('../db/connection');
+const favouriteProductsFn = require('../db/queries/getFavourites');
+const addNewFavouriteProductsFn = require('../db/queries/addToFavorite');
+const queryUser = require('../db/queries/getUserByID');
+
+
+//Handles any post requests to add-to-favorites
+router.post("/:productId/:userId", (req, res) => {
+  const cookieStored = req.session.user_id;
+  const displayNamePromise = queryUser.getUserByID(cookieStored);
+  const myFavouriteProducts = favouriteProductsFn.getFavourites(cookieStored);
+  const newFavorite = addNewFavouriteProductsFn.addNewFavorite(cookieStored, req.params.productId);
+
+  Promise.all([displayNamePromise, myFavouriteProducts, newFavorite])
+    .then(([]) => {
+      return res.redirect('/favourites');
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
+
+
+module.exports = router;
