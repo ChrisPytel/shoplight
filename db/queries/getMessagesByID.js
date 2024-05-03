2a96c6870d9438c194d2f316952b677292b8323a
ChrisPytel <chris_pytel@hotmail.com>
2024-05-03 00:58:54 -0400

    update getMessagesByID query to return inbox items by the most recent date of message creation

--- db/queries/getMessagesByID.js
+++ db/queries/getMessagesByID.js
@@ -7,7 +7,8 @@ const getMessagesByID = (id) => {
   FROM messages
   LEFT JOIN users ON messages.user_id_from = users.id
   LEFT JOIN products ON messages.product_id = products.id
-  WHERE user_id_to = $1;`;
+  WHERE user_id_to = $1
+  ORDER BY date_sent DESC;`;
   return db.query(queryString,[id])
     .then(data => {
       console.log(`Our DB returned: `, data.rows);
