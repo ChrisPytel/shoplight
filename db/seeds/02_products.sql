71df9361bca5a1db23209ff8122cb01487e75497
ChrisPytel <chris_pytel@hotmail.com>
2024-05-04 12:02:22 -0400

    update images in database

--- db/seeds/02_products.sql
+++ db/seeds/02_products.sql
@@ -1,29 +1,69 @@
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
-VALUES (2, 'Vintage Leather Messenger Bag', 'Handcrafted vintage-style leather messenger bag, perfect for carrying laptops and documents. Made from genuine leather with adjustable shoulder straps.', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQO9jg-ct78uuHlUcsVGEOsLhjRCeQvCoiOI7OvmFmdvA&s', 120, true, true);
+VALUES (2, 'Vintage Leather Messenger Bag', 'Handcrafted vintage-style leather messenger bag, perfect for carrying laptops and documents. Made from genuine leather with adjustable shoulder straps.', 'https://i.imgur.com/sRM4sAh.jpeg', 150, true, true);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
-VALUES (2, 'Wireless Bluetooth Headphones', 'High-quality wireless headphones with Bluetooth 5.0 technology. Features noise cancellation and long battery life, perfect for music lovers on the go.', 'https://m.media-amazon.com/images/I/41JACWT-wWL._AC_UF894,1000_QL80_.jpg', 80, false, true);
+VALUES (2, 'High End Studio Headphones', 'High-quality studio headphones with Bluetooth 5.0 technology. Features noise cancellation and long battery life, perfect for music lovers on the go. Great electronics inside', 'https://i.imgur.com/LWYYv16.jpeg', 320, false, true);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
-VALUES (1, 'Professional Chef Knife Set', 'Premium-quality chef knife set with ergonomic handles and razor-sharp blades. Includes a chef''s knife, bread knife, utility knife, and paring knife.', 'https://m.media-amazon.com/images/I/81DtItlIaIL._AC_UF1000,1000_QL80_.jpg', 150, true, true);
+VALUES (7, 'Professional Chef Knife Set', 'Premium-quality chef knife set with ergonomic handles and razor-sharp blades. Includes a chef''s knife, bread knife, utility knife, and paring knife.', 'https://i.imgur.com/eku6hcG.jpeg', 140, true, true);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
-VALUES (3, 'Smart Home Security Camera', 'State-of-the-art security camera with 1080p HD resolution and motion detection. Connects to your smartphone for remote monitoring and alerts.', 'https://m.media-amazon.com/images/I/61hyoHjWJAL._AC_UF894,1000_QL80_.jpg', 100, false, true);
+VALUES (3, 'Doggy Carry Bag (Dog not included)', 'Transport your furry friend in comfort and style with this gently used doggy carry bag, perfect for travel and outings.', 'https://i.imgur.com/3p9mq8W.jpeg', 60, true, false);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
-VALUES (4, 'Electric Handheld Milk Frother', 'Stainless steel handheld milk frother for making creamy froth in seconds. Ideal for lattes, cappuccinos, and hot chocolates.', 'https://www.unicornsuperfoods.com/cdn/shop/products/HANDHELDMILKFROTHER.jpg?v=1661229231', 25, true, true);
+VALUES (3, 'Hybrid Road Bike', 'This well-maintained road bike offers a smooth ride, perfect for cycling enthusiasts and fitness buffs alike.', 'https://i.imgur.com/vkEPTXU.jpeg', 1200, true, false);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
-VALUES (3, 'Reusable Stainless Steel Straws', 'Set of 8 eco-friendly stainless steel straws with cleaning brushes and carrying pouch. Durable and dishwasher safe, perfect for reducing plastic waste.', 'https://m.media-amazon.com/images/I/81pZ71cKNnL.jpg', 15, false, false);
-
-INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
-VALUES (1, 'Outdoor Waterproof Backpack', 'Rugged waterproof backpack with multiple compartments and padded shoulder straps. Suitable for hiking, camping, and outdoor adventures.', 'https://cdni.llbean.net/is/image/wim/510683_28180_41?scl=2', 70, true, true);
+VALUES (3, 'Nintendo Switch OLED', 'Grab this Nintendo Switch in excellent secondhand condition! Perfect for gaming on the go or at home, Ill throw in my copy of Luigis Mansion 3.', 'https://i.imgur.com/ju8EedC.jpeg', 320, false, false);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
 VALUES (2, 'Fitness Tracker Smartwatch', 'Multifunctional fitness tracker smartwatch with heart rate monitor, sleep tracker, and activity tracking. Syncs with your smartphone for notifications and updates.', 'https://m.media-amazon.com/images/I/71JU-bUt-sL.jpg', 50, false, true);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
-VALUES (9, 'Portable External Battery Charger', 'Compact and lightweight external battery charger with fast charging technology. Provides multiple charges for smartphones and other devices on the go.', 'https://media.wired.com/photos/6504b2a1afe02332db973557/master/w_960,c_limit/Ugreen_Power_Bank-SOURCE-Ugreen-Gear.jpg', 30, true, false);
+VALUES (7, 'Smartwatch Fitness tracker', 'Keep track of your fitness journey with this advanced fitness tracker. Monitor your workouts, heart rate, and sleep patterns with precision. Stay motivated and achieve your health goals with ease', 'https://i.imgur.com/0m5t9k5.jpeg', 180, true, true);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
-VALUES (8, 'Organic Bamboo Cutting Board Set', 'Set of 3 organic bamboo cutting boards in various sizes. Naturally antibacterial and easy to clean, perfect for food preparation.', 'https://m.media-amazon.com/images/I/81fdi6-lvoL._AC_UF1000,1000_QL80_.jpg', 40, false, true);
+VALUES (1, 'Yamaha Acoustic Guitar', 'Unleash your musical talent with this classic acoustic guitar. Crafted with quality materials, it delivers rich, resonant tones for a captivating sound experience.', 'https://i.imgur.com/ppmwMUG.jpeg', 180, false, true);
+
+INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
+VALUES (1, 'Rare Sony Trinitron TV', 'Step back in time with this rare CRT TV, in excellent working condition, ideal for retro gaming or adding a vintage touch to your home', 'https://i.imgur.com/H9Ibg4T.jpeg', 470, false, true);
+
+INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
+VALUES (5, 'Luxury Handbag', 'Elevate your fashion statement with this high-quality luxury handbag, crafted with precision and elegance to complement any outfit.', 'https://i.imgur.com/e0aRQfp.jpeg', 330, true, false);
+
+INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
+VALUES (2, 'Extra Large Abstract Painting', 'Add a touch of artistic flair to your space with this captivating large abstract painting, a conversation starter for art enthusiast.', 'https://i.imgur.com/ereSn1o.jpeg', 240, false, true);
+
+INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
+VALUES (3, 'UK Harry Potter Book Collection', 'Delve into the magical world of Harry Potter with this complete book collection, a treasure trove for fans of the beloved wizarding series.', 'https://i.imgur.com/9hFmGN9.jpeg', 120, false, true);
+
+INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
+VALUES (9, 'Compact Treadmill', 'Stay fit and active with this space-saving compact treadmill, designed for convenience and effective workouts in the comfort of your home.', 'https://i.imgur.com/6bVdqGt.jpeg', 440, false, true);
+
+INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
+VALUES (1, 'Portable Powerbank', 'Keep your devices powered up on the go with this reliable battery charger. Its compact, fast-charging, and compatible with various devices.', 'https://i.imgur.com/UISjNDU.jpeg', 90, false, true);
+
+INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
+VALUES (2, 'Organic Bamboo Cutting Board Set', 'Set of 4 organic bamboo cutting boards in various sizes. Naturally antibacterial and easy to clean, perfect for food preparation.', 'https://i.imgur.com/x1YfhJS.jpeg', 40, false, true);
+
+INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
+VALUES (3, 'Laptop Carry Bag', 'Safeguard your laptop in style with this durable carry bag. Designed for comfort and convenience, it features ample storage space for accessories.', 'https://m.media-amazon.com/images/I/81fdi6-lvoL._AC_UF1000,1000_QL80_.jpg', 120, false, true);
+
+INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
+VALUES (4, 'Video Game Collection', 'Dive into nostalgia with this GameCube collection, featuring classic titles that defined a generation of gaming.', 'https://i.imgur.com/NVjP7x7.jpeg', 140, false, true);
+
+INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
+VALUES (6, 'Smart Home Security Camera', 'State-of-the-art security camera with 1080p HD resolution and motion detection. Connects to your smartphone for remote monitoring and alerts.', 'https://i.imgur.com/ttv0yla.jpeg', 100, false, true);
+
+INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
+VALUES (7, '2000s Dvd Collection', 'Relive the best of the 2000s with this curated set of DVDs. From iconic movies to beloved TV series, this collection offers hours of entertainment and nostalgia', 'https://i.imgur.com/dGoqurg.jpeg', 130, false, true);
+
+INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
+VALUES (4, 'Electric Handheld Milk Frother', 'Stainless steel handheld milk frother for making creamy froth in seconds. Ideal for lattes, cappuccinos, and hot chocolates.', 'https://i.imgur.com/bpFr0UZ.jpeg', 25, true, true);
+
+INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
+VALUES (9, 'Outdoor Camping Bags', 'Discover the ultimate companion for your outdoor expeditions with our rugged camping backpacks. Designed for durability and comfort.', 'https://i.imgur.com/bQ8IhY4.jpeg', 140, false, true);
+
+
+INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
+VALUES (6, 'Smartwatch', 'Elevate your lifestyle with our cutting-edge smartwatch. Seamlessly blend fashion with functionality as you stay connected, track fitness goals, and manage notifications from your wrist.', 'https://i.imgur.com/WmnAshU.jpeg', 70, true, true);
