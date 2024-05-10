a23b937b4c9245f297e623c95552d14253220a82
ChrisPytel <chris_pytel@hotmail.com>
2024-05-09 21:39:03 -0400

    update seed data for products, favorites, and messages to reflect realistic user situations

--- db/seeds/02_products.sql
+++ db/seeds/02_products.sql
@@ -2,31 +2,28 @@ INSERT INTO products (user_id, name, description, photo_url, price, is_featured,
 VALUES (2, 'Vintage Leather Messenger Bag', 'Handcrafted vintage-style leather messenger bag, perfect for carrying laptops and documents. Made from genuine leather with adjustable shoulder straps.', 'https://i.imgur.com/sRM4sAh.jpeg', 150, true, true);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
-VALUES (2, 'High End Studio Headphones', 'High-quality studio headphones with Bluetooth 5.0 technology. Features noise cancellation and long battery life, perfect for music lovers on the go. Great electronics inside', 'https://i.imgur.com/LWYYv16.jpeg', 320, false, true);
+VALUES (4, 'High End Studio Headphones', 'High-quality studio headphones with Bluetooth 5.0 technology. Features noise cancellation and long battery life, perfect for music lovers on the go. Great electronics inside', 'https://i.imgur.com/LWYYv16.jpeg', 320, false, true);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
-VALUES (7, 'Professional Chef Knife Set', 'Premium-quality chef knife set with ergonomic handles and razor-sharp blades. Includes a chef''s knife, bread knife, utility knife, and paring knife.', 'https://i.imgur.com/eku6hcG.jpeg', 140, true, true);
+VALUES (7, 'Professional Chef Knife Set', 'Premium-quality chef knife set with ergonomic handles and razor-sharp blades. Includes a chef''s knife, bread knife, utility knife, and paring knife.', 'https://i.imgur.com/eku6hcG.jpeg', 140, false, true);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
-VALUES (3, 'Doggy Carry Bag (Dog not included)', 'Transport your furry friend in comfort and style with this gently used doggy carry bag, perfect for travel and outings.', 'https://i.imgur.com/3p9mq8W.jpeg', 60, true, false);
+VALUES (3, 'Doggy Carry Bag (Dog not included)', 'Transport your furry friend in comfort and style with this gently used doggy carry bag, perfect for travel and outings.', 'https://i.imgur.com/3p9mq8W.jpeg', 60, false, true);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
-VALUES (3, 'Hybrid Road Bike', 'This well-maintained road bike offers a smooth ride, perfect for cycling enthusiasts and fitness buffs alike.', 'https://i.imgur.com/vkEPTXU.jpeg', 1200, true, false);
+VALUES (1, 'Hybrid Road Bike', 'This well-maintained road bike offers a smooth ride, perfect for cycling enthusiasts and fitness buffs alike.', 'https://i.imgur.com/vkEPTXU.jpeg', 1200, true, true);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
-VALUES (3, 'Nintendo Switch OLED', 'Grab this Nintendo Switch in excellent secondhand condition! Perfect for gaming on the go or at home, Ill throw in my copy of Luigis Mansion 3.', 'https://i.imgur.com/ju8EedC.jpeg', 320, false, false);
+VALUES (5, 'Nintendo Switch OLED', 'Grab this Nintendo Switch in excellent secondhand condition! Perfect for gaming on the go or at home, Ill throw in my copy of Luigis Mansion 3.', 'https://i.imgur.com/ju8EedC.jpeg', 320, false, false);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
-VALUES (2, 'Fitness Tracker Smartwatch', 'Multifunctional fitness tracker smartwatch with heart rate monitor, sleep tracker, and activity tracking. Syncs with your smartphone for notifications and updates.', 'https://m.media-amazon.com/images/I/71JU-bUt-sL.jpg', 50, false, true);
-
-INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
-VALUES (7, 'Smartwatch Fitness tracker', 'Keep track of your fitness journey with this advanced fitness tracker. Monitor your workouts, heart rate, and sleep patterns with precision. Stay motivated and achieve your health goals with ease', 'https://i.imgur.com/0m5t9k5.jpeg', 180, true, true);
+VALUES (7, 'Smartwatch Fitness tracker', 'Keep track of your fitness journey with this advanced fitness tracker. Monitor your workouts, heart rate, and sleep patterns with precision. Stay motivated and achieve your health goals with ease', 'https://i.imgur.com/0m5t9k5.jpeg', 180, false, false);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
 VALUES (1, 'Yamaha Acoustic Guitar', 'Unleash your musical talent with this classic acoustic guitar. Crafted with quality materials, it delivers rich, resonant tones for a captivating sound experience.', 'https://i.imgur.com/ppmwMUG.jpeg', 180, false, true);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
-VALUES (1, 'Rare Sony Trinitron TV', 'Step back in time with this rare CRT TV, in excellent working condition, ideal for retro gaming or adding a vintage touch to your home', 'https://i.imgur.com/H9Ibg4T.jpeg', 470, false, true);
+VALUES (1, 'Rare Sony Trinitron CRT TV', 'Step back in time with this rare CRT TV, in excellent working condition, ideal for retro gaming or adding a vintage touch to your home', 'https://i.imgur.com/H9Ibg4T.jpeg', 470, false, true);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
 VALUES (5, 'Luxury Handbag', 'Elevate your fashion statement with this high-quality luxury handbag, crafted with precision and elegance to complement any outfit.', 'https://i.imgur.com/e0aRQfp.jpeg', 330, true, false);
@@ -41,16 +38,16 @@ INSERT INTO products (user_id, name, description, photo_url, price, is_featured,
 VALUES (9, 'Compact Treadmill', 'Stay fit and active with this space-saving compact treadmill, designed for convenience and effective workouts in the comfort of your home.', 'https://i.imgur.com/6bVdqGt.jpeg', 440, false, true);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
-VALUES (1, 'Portable Powerbank', 'Keep your devices powered up on the go with this reliable battery charger. Its compact, fast-charging, and compatible with various devices.', 'https://i.imgur.com/UISjNDU.jpeg', 90, false, true);
+VALUES (10, 'Portable Powerbank', 'Keep your devices powered up on the go with this reliable battery charger. Its compact, fast-charging, and compatible with various devices.', 'https://i.imgur.com/UISjNDU.jpeg', 90, false, true);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
 VALUES (2, 'Organic Bamboo Cutting Board Set', 'Set of 4 organic bamboo cutting boards in various sizes. Naturally antibacterial and easy to clean, perfect for food preparation.', 'https://i.imgur.com/x1YfhJS.jpeg', 40, false, true);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
-VALUES (3, 'Laptop Carry Bag', 'Safeguard your laptop in style with this durable carry bag. Designed for comfort and convenience, it features ample storage space for accessories.', 'https://m.media-amazon.com/images/I/81fdi6-lvoL._AC_UF1000,1000_QL80_.jpg', 120, false, true);
+VALUES (1, 'Laptop Carry Bag', 'Safeguard your laptop in style with this durable carry bag. Designed for comfort and convenience, it features ample storage space for accessories.', 'https://i.imgur.com/Wn9BzMy.jpeg', 120, true, true);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
-VALUES (4, 'Video Game Collection', 'Dive into nostalgia with this GameCube collection, featuring classic titles that defined a generation of gaming.', 'https://i.imgur.com/NVjP7x7.jpeg', 140, false, true);
+VALUES (4, 'Gamecube Classics Collection', 'Dive into nostalgia with this GameCube collection, featuring classic titles that defined a generation of gaming.', 'https://i.imgur.com/NVjP7x7.jpeg', 140, true, true);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
 VALUES (6, 'Smart Home Security Camera', 'State-of-the-art security camera with 1080p HD resolution and motion detection. Connects to your smartphone for remote monitoring and alerts.', 'https://i.imgur.com/ttv0yla.jpeg', 100, false, true);
@@ -59,11 +56,10 @@ INSERT INTO products (user_id, name, description, photo_url, price, is_featured,
 VALUES (7, '2000s Dvd Collection', 'Relive the best of the 2000s with this curated set of DVDs. From iconic movies to beloved TV series, this collection offers hours of entertainment and nostalgia', 'https://i.imgur.com/dGoqurg.jpeg', 130, false, true);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
-VALUES (4, 'Electric Handheld Milk Frother', 'Stainless steel handheld milk frother for making creamy froth in seconds. Ideal for lattes, cappuccinos, and hot chocolates.', 'https://i.imgur.com/bpFr0UZ.jpeg', 25, true, true);
+VALUES (4, 'Electric Handheld Milk Frother', 'Stainless steel handheld milk frother for making creamy froth in seconds. Ideal for lattes, cappuccinos, and hot chocolates.', 'https://i.imgur.com/bpFr0UZ.jpeg', 25, false, true);
 
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
 VALUES (9, 'Outdoor Camping Bags', 'Discover the ultimate companion for your outdoor expeditions with our rugged camping backpacks. Designed for durability and comfort.', 'https://i.imgur.com/bQ8IhY4.jpeg', 140, false, true);
 
-
 INSERT INTO products (user_id, name, description, photo_url, price, is_featured, is_available) 
-VALUES (6, 'Smartwatch', 'Elevate your lifestyle with our cutting-edge smartwatch. Seamlessly blend fashion with functionality as you stay connected, track fitness goals, and manage notifications from your wrist.', 'https://i.imgur.com/WmnAshU.jpeg', 70, true, true);
+VALUES (6, 'Smartwatch', 'Elevate your lifestyle with our cutting-edge smartwatch. Seamlessly blend fashion with functionality as you stay connected, track fitness goals, and manage notifications from your wrist.', 'https://i.imgur.com/WmnAshU.jpeg', 70, false, false);
--- db/seeds/04_favourites.sql
+++ db/seeds/04_favourites.sql
@@ -1,10 +1,17 @@
-INSERT INTO favourites (user_id, product_id) VALUES (1, 1);
+INSERT INTO favourites (user_id, product_id) VALUES (1, 8);
 INSERT INTO favourites (user_id, product_id) VALUES (1, 9);
-INSERT INTO favourites (user_id, product_id) VALUES (2, 2);
-INSERT INTO favourites (user_id, product_id) VALUES (3, 4);
-INSERT INTO favourites (user_id, product_id) VALUES (4, 5);
-INSERT INTO favourites (user_id, product_id) VALUES (5, 1);
-INSERT INTO favourites (user_id, product_id) VALUES (6, 3);
-INSERT INTO favourites (user_id, product_id) VALUES (7, 4);
-INSERT INTO favourites (user_id, product_id) VALUES (8, 2);
-INSERT INTO favourites (user_id, product_id) VALUES (9, 5);
+
+INSERT INTO favourites (user_id, product_id) VALUES (2, 4);
+INSERT INTO favourites (user_id, product_id) VALUES (2, 7);
+INSERT INTO favourites (user_id, product_id) VALUES (2, 15);
+
+INSERT INTO favourites (user_id, product_id) VALUES (3, 5);
+INSERT INTO favourites (user_id, product_id) VALUES (3, 5);
+
+INSERT INTO favourites (user_id, product_id) VALUES (4, 3);
+INSERT INTO favourites (user_id, product_id) VALUES (5, 11);
+INSERT INTO favourites (user_id, product_id) VALUES (6, 2);
+INSERT INTO favourites (user_id, product_id) VALUES (7, 7);
+INSERT INTO favourites (user_id, product_id) VALUES (8, 13);
+INSERT INTO favourites (user_id, product_id) VALUES (9, 18);
+INSERT INTO favourites (user_id, product_id) VALUES (10, 10);
